2f093391c034cfb235b52101e8344b088cbe046b
catiadelgado <a25665@alunos.isel.pt>
2019-11-11 14:49:24 +0000

    falta terminar o ultimo exercicio da aula 3 e fazer merge com o que ja tinha feito anteriormente, mas que não tinha feito push

--- src/aula3.java
+++ src/aula3.java
@@ -5,15 +5,26 @@ public class aula3 {
 		return n*factorial(n-1);
 	}
 	
-	// nao percebi bem como este algoritmo funciona
+
 	static int gcd ( int a , int b ){
 		if ( b==0) return a; 
 		else 
 			return gcd( b,a%b);
-		
-			
-			
 	}
+	
+	//exercicio extra-A
+	static int largerDifferenceBetweenPrimes ( int n){
+		//TODO fazer o metodo usando o isPrime1
+		return 0; 
+	}
+		/*
+		 * Defina uma função que recebe como argumento um número natural n e devolve a maior diferença entre dois números primos consecutivos até n.
+Exemplo:
+
+largerDifferenceBetweenPrimes(13) → 4     (a diferença entre 7 e 11)
+
+		 * */	
+	
 	
 
 }
